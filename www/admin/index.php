3a6f79482d27dc118bfbe6ac8164425c3a2f46c6
liesh835 <liesh835@student.otago.ac.nz>
2020-08-28 13:56:13 +1200

    Added sql query and the display of an order table in admin server, also added CSS file for styling.

--- www/admin/index.php
+++ www/admin/index.php
@@ -1,12 +1,59 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>Test page</title>
-</head>
-<body>
+<?php
+   $scriptList = array('js/jquery-3.5.1.min.js');
+   include('private/header.php');
+   include('private/sql.php');
+   ?>
 
-<h1>Testing 1, 2, 3 ...</h1>
-<p>Hello from Admin.</p>
+<section id="purchases">
+  <?php
+     $sql = "SELECT * FROM orders";
+     $result = $db->query($sql);
+  ?>
+  
+  <div class = "table">
+    <table class = "order_table">
+      <tr>
+        <th>Order ID</th>
+        <th>Customer Name</th>
+        <th>Customer Email</th>
+        <th>Customer Address</th>
+        <th>Item ID</th>
+        // <th>Item Name</th>
+        // <th>Item Price</th>
+        <th>Order Status</th>
+      </tr>
 
-</body>
-</html>
+      <?php
+         
+         if ($result->num_rows > 0) {
+      while ($purchases = $result->fetch_assoc()) {
+      
+      $name = $purchases['cust_name'];
+      $email = $purchases['cust_email'];
+      $address = $purchases['cust_address'];
+      $orderId = $purchases['order_id'];
+      $itemId = $purchases['item_id'];
+      $status = $purchases['shipped'];
+      
+      ?>
+      
+      <tr>
+        <td><?php echo $orderId; ?></td>
+        <td><?php echo $name; ?></td>
+        <td><?php echo $email; ?></td>
+        <td><?php echo $address; ?></td>
+        <td><?php echo $itemId; ?></td>
+        <td><?php echo $status; ?></td>
+      </tr><?php
+              }
+              } else {
+              echo "fail";
+              }
+              ?>
+    </table>
+  </div>
+</section>
+
+$(document).ready(function() {
+$()
+}
